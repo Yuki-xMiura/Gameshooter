2f359e34d30438310d68883fa7ad5a15f2ca83eb
Yuki-iMiura <139273337+Yuki-iMiura@users.noreply.github.com>
2026-02-16 11:12:20 -0300

    Create Project

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+print("Setup Start")
+pygame.init()
+window = pygame.display.set_mode(size=(800, 600))
+print("Setup End")
+
+print('Loop Start')
+While True:
+    # Check for all events
+    for event in pygame.event.get():
+        # If the event is QUIT then exit the program
+        if event.type == pygame.QUIT:
+            print('Loop End')
+            pygame.quit()
+            exit()
