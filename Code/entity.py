c387a8a3d5205d761950891dbb4bdcfe756d4cf5
Yuki-iMiura <139273337+Yuki-iMiura@users.noreply.github.com>
2026-02-19 22:04:55 -0300

    apply convert_alpha function  create level_text function, with time, fps and number of entity  set standart fps game

--- Code/entity.py
+++ Code/entity.py
@@ -10,7 +10,7 @@ from code.Paraments import ENTITY_SPEED
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load('./asset/' + name + '.png')
+        self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = ENTITY_SPEED[self.name]
     
--- Code/level.py
+++ Code/level.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from tkinter.font import Font
+
 import pygame
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory  
+from code.Paraments import C_WHITE, ALTURA
 
 
 class Level:
     def __init__(self, window, name, game_mode):
+        self.timeout = 20000
         self.window = window
         self.name = name
         self.game_mode = game_mode
@@ -15,7 +19,11 @@ class Level:
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
     def run(self, ):
+        pygame.mixer.music.load(f'asset/{self.name}.mp3')
+        pygame.mixer.music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
@@ -30,3 +38,15 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+            
+             # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', C_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, ALTURA - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, ALTURA - 20))
+            pygame.display.flip()
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: pygame.Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: pygame.Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
